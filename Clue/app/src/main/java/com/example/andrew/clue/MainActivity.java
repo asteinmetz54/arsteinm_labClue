2b310411cf31c71231cc9653a44ff8700259e151
Andrew Steinmetz <asteinmetz54@gmail.com>
2016-11-29 14:10:10 -0800

    load html file

--- Clue/app/src/main/java/com/example/andrew/clue/MainActivity.java
+++ Clue/app/src/main/java/com/example/andrew/clue/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.andrew.clue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView browser = (WebView)findViewById(R.id.web_view);
+        browser.loadUrl("file:///android_asset/clue.html");
     }
 }
